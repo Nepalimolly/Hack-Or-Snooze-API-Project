486dca5dfabedd4299af7270c28f4076ade64f9c
Nepalimolly <121688585+Nepalimolly@users.noreply.github.com>
2023-07-12 16:30:43 -0500

    project commit

---
+++ js/nav.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+  $storiesContainer.hide();
+}
+
+$navLogin.on("click", navLoginClick);
+
+function navSubmitStoryClick(evt) {
+  hidePageComponents();
+  $allStoriesList.show();
+  $submitStoryForm.show();
+}
+$navSubmitStory.on("click", navSubmitStoryClick);
+
+function navFavoritesClick(evt) {
+  hidePageComponents();
+  putFavoritesListOnPage();
+}
+
+$body.on("click", "#nav-favorites", navFavoritesClick);
+
+function navMyStories(evt) {
+  hidePageComponents();
+  putUserStoriesOnPage();
+  $ownStories.show();
+}
+
+$body.on("click", "#nav-my-stories", navMyStories);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
